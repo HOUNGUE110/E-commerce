8b403fb9b366f0abf047b85f4cbcf304e0db8c07
HOUNGUE110 <adigedeon6@gmail.com>
2025-09-07 21:58:15 +0000

    all commit

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-const boutonsAjouter = document.querySelectorAll('.produit button');
+const boutonsAjouter = document.querySelectorAll('.AjoutPanier');
 const panier = [];
 
 boutonsAjouter.forEach(bouton => {
     bouton.addEventListener('click', () => {
-        alert('Produit ajouté au panier');
-        //const produit = bouton.parentElement;
-        //const nom = produit.querySelector('h3').textContent;
-        //const prix = parseFloat(produit.querySelector('p:last-child').textContent.split(' : ')[1]);
-
-        //panier.push({ nom, prix });
-        //console.log(panier); 
-        //document.querySelector('#panier').textContent = `Panier : ${panier.reduce((acc, curr) => acc + curr.prix, 0).toFixed(2)} ���`; 
-        
+        //alert('Produit ajouté au panier');
+        const produit = bouton.parentElement;
+        const nom = produit.querySelector('.produit:data-nom').textContent;
+        const prix = parseFloat(produit.querySelector('.produit:data-prix').textContent.split(' : ')[1]);
+        panier.push({ nom, prix });
+        console.log(panier);
+        document.querySelector('#totalPanier').textContent = `Panier : ${panier.reduce((acc, curr) => acc + curr.prix, 0).toFixed(2)} €`;
     });
-    
-});
 
-//Validation du panier
-const boutonValider = document.querySelector('#validerPanier');
+});
